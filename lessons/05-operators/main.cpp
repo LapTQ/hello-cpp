3802e1362be1d1418866525209a47e37fa3fe8e8
Tran Quoc Lap <56187517+LapTQ@users.noreply.github.com>
2025-01-15 10:07:15 +0000

    msg

--- lessons/05-operators/main.cpp
+++ lessons/05-operators/main.cpp
@@ -23,3 +23,40 @@
       `x = y = 5` is equivalent to `x = (y = 5)`, because the return value of `y = 5` is `y`.
 
 */
+
+#include <iostream>
+
+int main()
+{
+    int x{ 1 };
+    int y{ 2 };
+    int z{  };
+    int max{  };
+
+    // comma operator
+    std::cout << (++x, ++y) << '\n'; // increment x and y, evaluates to the right operand
+    // Avoid using the comma operator, except within for loops.
+    
+    // Note that comma has the lowest precedence of all the operators, even lower than assignment. 
+    z = (x, y); // evaluate (a, b) first to get result of b, then assign that value to variable z.
+    z = x, y; // evaluates as "(z = a), b", so z gets assigned the value of a, and b is evaluated and discarded.
+
+    // conditional operator
+    max = ((x > y) ? x : y);
+    std::cout << (x < 0) ? "negative" : "non-negative"; // you might expect this to print "non-negative", but it prints "0"
+    // because operator<< has higher precedence than operator?:
+
+    std::cout << (true ? 1 : 2) << '\n';    // okay: both operands have matching type int
+    std::cout << (false ? 1 : 2.2) << '\n'; // okay: int value 1 converted to double
+    std::cout << (true ? -1 : 2u) << '\n';  // surprising result: -1 converted to unsigned int, result out of range
+    std::cout << ((x != 5) ? x : "x is 5"); // compile error: compiler can't find common type for constexpr int and C-style string literal
+
+    return 0;
+}
+
+/* References
+
+- https://www.learncpp.com/cpp-tutorial/the-comma-operator/
+- https://www.learncpp.com/cpp-tutorial/the-conditional-operator/
+
+*/
