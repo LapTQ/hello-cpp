b995c1e9561b6de8f092e912af118a6515c61cac
LapTQ <lap.tq4@gmail.com>
2025-01-29 02:26:02 +0700

    msg

--- lessons/049-function-templates/main.cpp
+++ lessons/049-function-templates/main.cpp
@@ -1,12 +1,13 @@
 /* C++ templates
 
 - Unlike a normal definition (where all types must be specified).
-- Once a template is defined, the compiler can generate as many overloaded functions 
-  as needed, each using different actual types!
 - template code can be used with types that didn’t even exist when the template was written!
 
 - The initial function template is called the "primary template".
-  The functions generated from the primary template are called "specializations".
+  The functions generated from the primary template are called "specializations" (or "function instance").
+
+- Function templates are not actually functions -- their code isn’t compiled or executed directly. 
+  Instead, function templates have one job: to generate functions.
 */
 
 
@@ -19,9 +20,72 @@ T max(T x, T y)
 // The scope of a template parameter declaration is strictly limited to the function template that follows.
 
 
+/* Function template instantiation
+
+- To use our max<T> function template, follow the syntax:
+max<actual_type>(arg1, arg2);
+
+the addition of the type in angled brackets is called a "template argument".
+
+- The process of creating functions (with specific types) from function templates (with template types) is called "instantiation".
+- Implicit instantiation: instantiated due to a function call.
+
+- A function template is only instantiated the first time a function call is made in each translation unit. 
+  Further calls to the function are routed to the already instantiated function.
+*/
+
+#include <iostream>
+
+
+/* Template argument deduction
+
+- In cases where the type of the arguments match the actual type we want, we do not need to specify the actual type.
+
+- 2 ways:
+    ++ consider only template function overloads (with the empty angled brackets): print<>
+    ++ consider both template and non-template function overloads (with no angled brackets): print. In this case,
+       the non-template function will be "preferred".
+*/
+
+// generic
+template <typename T>
+void print(T x)
+{
+    std::cout << x;
+}
+
+// specialize how to handles printing of a bool
+void print(bool x)
+{
+    std::cout << std::boolalpha << x;
+}
+
+
+int main()
+{
+    std::cout << max<int>(1, 2) << '\n'; // instantiates and calls function max<int>(int, int)
+    std::cout << max<int>(4, 3) << '\n';    // calls already instantiated function max<int>(int, int)
+    std::cout << max<double>(1, 2) << '\n'; // instantiates and calls function max<double>(double, double)
+
+
+    // Template argument deduction
+    print<bool>(true); // calls print<bool>(bool) -- prints 1
+    std::cout << '\n';
+
+    print<>(true);     // deduces print<bool>(bool) (non-template functions not considered) -- prints 1
+    std::cout << '\n';
+
+    print(true);       // calls print(bool) -- prints true
+    // consider both template and non-template function overloads, but non-template function is preferred
+    std::cout << '\n';
+
+    return 0;
+}
+
+
 /* typename vs class
 
-- keyword typename (preferred) or class can be used to declare type template parameters.
+- keyword typename (preferred) or class can be used to declare "type template parameters"
 - There is no difference between them in this context, but we prefer typename because it makes
   it clearer that any type (such as fundamental type) can be used, not just class types.
 */
@@ -29,4 +93,5 @@ T max(T x, T y)
 /* References 
 
 - https://www.learncpp.com/cpp-tutorial/function-templates/
+- https://www.learncpp.com/cpp-tutorial/function-template-instantiation/
 */
