4a473170012bdbc6a99bcf731308efd33f787db4
LapTQ <lap.tq4@gmail.com>
2025-02-08 02:48:53 +0700

    msg

---
+++ lessons/093-std-vector-bool/main.cpp
@@ -0,0 +1,13 @@
+/* std::vector<bool> tradeoffs
+
+Read: https://web.archive.org/web/20250109204743/https://www.learncpp.com/cpp-tutorial/stdvector-bool/#:~:text=std%3A%3Avector%3Cbool%3E%20tradeoffs
+
+- Favor constexpr std::bitset, std::vector<char>, or 3rd party dynamic bitsets over std::vector<bool>.
+*/
+
+
+
+/* References
+
+- https://www.learncpp.com/cpp-tutorial/stdvector-bool/
+*/
