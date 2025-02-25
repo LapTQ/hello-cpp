59629c2d3abd53f7847c35c95c62459473dc8639
LapTQ <lap.tq4@gmail.com>
2025-02-25 08:13:06 +0700

    msg

--- lessons/079-classes-and-header-files/main.cpp
+++ lessons/079-classes-and-header-files/main.cpp
@@ -17,12 +17,32 @@
 - Member functions defined outside the class definition are not implicitly inline.
   => must be explicitly marked as inline if you want to define them in a header file.
   => why such functions are usually defined in a .cpp file.
+*/
 
-- For template classes, template member functions defined outside the class are almost always defined inside the header file, 
-  beneath the class definition. Because, the compiler needs to see the full template definition in order to instantiate it.
+
+/* Splitting up template classes
+
+- With non-template classes, the common procedure is to put the class definition in a header file, 
+  and the member function definitions in a similarly named code file. 
+  However, with templates, this does not work, we’ll get a linker error: https://www.learncpp.com/cpp-tutorial/template-classes/#:~:text=Splitting%20up%20template%20classes
+
+- Solution 1: Put the entire template class in a header file.
+    Downside: increase your compile and link times.
+- Solution 2: move the contents of .cpp to a new file named .inl, and include the .inl file 
+    at the end of the header file (inside the header guard).
+    That yields the same result as putting all the code in the header, but helps keep things a little more organized.
+- Solution 3: use a three-file approach: header file, code file, and explicit instantiation file.
+    For example: Array.h, Array.cpp, and templates.cpp.
+    
+    Other code files that want to use these types can include Array.h (to satisfy the compiler), 
+    and the linker will link in these explicit type definitions from template.cpp.
+
+    => more efficient, but requires more files.
 */
 
 /*
 
 - https://www.learncpp.com/cpp-tutorial/classes-and-header-files/
+- https://www.learncpp.com/cpp-tutorial/template-classes/
+
 */
---
+++ lessons/133-function-template-specialization/main.cpp
@@ -0,0 +1,11 @@
+/*
+- When instantiating a function template for a given type, a particular function will have the same implementation details for each instanced type.
+- However, there are cases where you may want to provide a slightly different implementation for a specific type.
+*/
+
+
+
+/* References
+
+- https://www.learncpp.com/cpp-tutorial/function-template-specialization/
+*/
